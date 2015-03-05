47fe96d3cd00d0c710d866d580ea13b8192d1373
Austin Meier <diveintotheword@att.net>
2015-03-05 14:23:08 -0600

    Initialize repository

---
+++ Driver.java
@@ -0,0 +1,13 @@
+
+public class Driver 
+{
+	public static void main(String[] args)
+	{
+		Person p1 = new Person("Person1");
+		Person p2 = new Person("Person2");
+		p1.display();
+		p2.display();
+	}
+	
+	
+}
