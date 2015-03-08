5bdcd721f97af222944d410fde5fc5c7f87c75a7
Austin Meier <diveintotheword@att.net>
2015-03-08 18:11:55 -0500

    HW 9 finished

--- Driver.java
+++ Driver.java
@@ -2,12 +2,11 @@ public class Driver
 {
 	public static void main(String[] args) throws Exception
 	{
-		Queue q= new Queue();	//Create a Queue like creating a stack then push 
+		Queue q= new Queue();
 		for(int i = 1; i < 101; i++)
 		{
 			Person p = new Person("Person " + i);
 			q.enqueue(p);
-			p.display();
 		}
 		for (int j = 1; j <101; j++)
 		{
